2df7c1c444b0f39763d77b728060675edb9ec506
Kristina Mujakic <100082094+kristinaMujakic@users.noreply.github.com>
2022-02-26 10:58:02 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,59 @@
+const userName = "Kristina";
+
+const getUserChoice = function (userInput) {
+  userInput = userInput.toLowerCase();
+  if (
+    userInput === "rock" ||
+    userInput === "paper" ||
+    userInput === "scissors"
+  ) {
+    return userInput;
+  } else {
+    console.log("ERROR");
+  }
+};
+
+const getComputerChoice = function () {
+  let randomNum = Math.floor(Math.random() * 3);
+  if (randomNum === 0) {
+    return "rock";
+  } else if (randomNum === 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+};
+
+const determineWinner = function (userChoice, computerChoice) {
+  if (userChoice === computerChoice) {
+    return "It's a tie!";
+  } else if (userChoice === "rock") {
+    if (computerChoice === "paper") {
+      return "Computer won!";
+    } else {
+      return `Congrats ${userName}, you won!`;
+    }
+  } else if (userChoice === "paper") {
+    if (computerChoice === "scissors") {
+      return "Computer won!";
+    } else {
+      return `Congrats ${userName}, you won!`;
+    }
+  } else if (userChoice === "scissors") {
+    if (computerChoice === "rock") {
+      return "Computer won!";
+    } else {
+      return `Congrats ${userName}, you won!`;
+    }
+  }
+};
+
+const playGame = function () {
+  const userChoice = getUserChoice("paper");
+  console.log(userChoice);
+  const computerChoice = getComputerChoice();
+  console.log(computerChoice);
+  console.log(determineWinner(userChoice, computerChoice));
+
+  return playGame();
+};
